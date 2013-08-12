9623d3b2d5099341d817b5742be71c3d11ee4233
Erica Xu <erica.xu@uwaterloo.ca>
2013-08-11 23:43:30 -0400

    Added loading screen using Spin.js library.

--- js/ui.js
+++ js/ui.js
@@ -10,6 +10,12 @@ $(document).ready(function() {
 	var $subjectResults = $("#subject-results");
 	var $courseResults = $("#course-results");
 
+
+	var spinner;
+	var spinnerOption = {
+		lines: 11, length: 14, width: 5, radius: 21, corners: 1, rotate: 0, direction: 1, color: '#222', speed: 1, trail: 42, shadow: false, hwaccel: false, className: 'spinner', zIndex: 2e9, top: '100', left: 'auto'
+	};
+
 	var subjectToIconMap = {
 		"AFM": "money", "ACTSC": "bar-chart", "ANTH": "man-woman", "AHS": "dropper", "APPLS": "japanese", "AMATH": "calculator", "ARCH": "tower", "ARTS": "pen", "ARBUS": "business-person", "AVIA": "airplane", "BIOL": "microscope", "BUS": "business-person", "BET": "idea", "CHE": "fire", "CHEM": "beaker", "CHINA": "chinese", "CMW": "church", "CIVE": "road", "CLAS": "ankh", "CO": "puzzle", "COMM": "money", "CS": "console", "COOP": "work", "CROAT": "translation", "DAC": "film", "DRAMA": "mask", "DUTCH": "translation", "EARTH": "earth", "EASIA": "china-map", "ECON": "line-chart", "ECE": "chip", "ENGL": "pen", "ESL": "translation", "ENBUS": "recycle", "ERS": "recycle", "ENVE": "recycle", "ENVS": "recycle", "FINE": "palette", "FR": "translation", "GENE": "hard-hat", "GEOG": "globe", "GEOE": "mountain", "GER": "translation", "GERON": "aging", "GBDA": "film", "GRK": "translation", "HLTH": "first-aid", "HRM": "people", "HUMSC": "man-woman", "INDEV": "earth", "INTST": "earth", "INTTS": "earth", "ITAL": "translation", "JAPAN": "japanese", "JS": "jewish", "KIN": "run", "KOREA": "translation", "LAT": "translation", "LS": "gavel", "MATBUS": "business-person", "MSCI": "organize", "MNS": "atom", "MATH": "calculator", "MTHEL": "calculator", "ME": "gear", "MTE": "gear", "MEDVL": "ankh", "MUSIC": "music", "NE": "atom", "NATST": "native", "OPTOM": "eye", "PACS": "peace", "PHARM": "pill", "PHIL": "thinking", "PHYS": "atom", "PLAN": "plan", "POLSH": "translation", "PSCI": "congress", "PORT": "translation", "PD": "wtf", "PDPHRM": "wtf", "PSYCH": "brain", "PMATH": "infinity", "REC": "island", "RS": "church", "RUSS": "translation", "SCI": "magnet", "SCBUS": "magnet", "SMF": "man-woman", "SDS": "network", "SOCWK": "network", "SWREN": "network", "STV": "network",  "SOC": "network", "SE": "console", "SPAN": "translation", "SPCOM": "speech", "STAT": "bar-chart", "SI": "islam", "SYDE": "rocket", "UNIV": "goose", "VCULT": "film", "WS": "female", "WKRPT": "wtf"
 	};
@@ -142,6 +148,13 @@ $(document).ready(function() {
 		} else if (component === "small-course") {
 			return "<a href='" + id + "'><div class='course small " + BACKEND.subjects[subject].department + "' id='" + id + "'><div class='header'>" + icon + "<div class='title'><h2><span class='code'>" + subject + " " + number + "</span></h2></div></div></div></a>";
 		}
+	}			
+
+	function stopLoadingScreen () {
+		spinner.stop();
+		$searchBar.show();
+		$searchResults.show();
+		init();
 	}
 
 	function init () {
@@ -180,6 +193,7 @@ $(document).ready(function() {
 		work();
 		window.addEventListener("popstate", work);
 	}
-	
-	init();
+
+	spinner = new Spinner(spinnerOption).spin(document.getElementById("loading-screen"));
+	BACKEND.registerUICallback(stopLoadingScreen);
 });
